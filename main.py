dca14bdf63da7e8c680b693e8f5087d9642f22a4
Alan <alan@remote.com>
2020-10-06 10:18:26 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import *
+from flask_cors import *
+
+# runtime configurations
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "<h1>hello world<h1>"
+
+
+def main():
+    CORS(app)
+    app.run()
+
+
+main()
