b85c56e5e53642d4ac0b7d83136078f6b8168e20
Alan <alan@remote.com>
2020-10-06 13:18:02 -0500

    added another get mapping

--- app.py
+++ app.py
@@ -12,6 +12,21 @@ def index():
     return "<h1>hello world<h1>"
 
 
+@app.route('/tutor', methods=['GET'])
+def get_tutor():
+    return {
+        "name": "omruti",
+        "fav subjects":
+            [
+                "research",
+                "data science",
+                "sql",
+                "literature"
+            ]
+    }
+
+
 if __name__ == '__main__':
     CORS(app)
+    app.debug = True
     app.run()
