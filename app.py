63266d0f34c1306314f3211b39448d71543623f6
Alan <alan@remote.com>
2020-11-07 19:01:19 -0600

    put and delete methods

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from mongo.MongoDB import *
 
 USERNAME = PASSWORD = "mongo"
 DB = "sandboxDB"
-COLLECTIONS = {"sandbox" : AUTO_INCREMENT}
+COLLECTIONS = {"sandbox": AUTO_INCREMENT}
 URL = f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.u6lhh.mongodb.net/{DB}?retryWrites=true&w=majority"
 
 mongo = MongoDB(url=URL, database=DB, collections=COLLECTIONS)
@@ -16,39 +16,39 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "<h1>heroku flask app</h1>" \
-           "<p> To test mongo db functionalities, open " \
-           "Postman and use the following url patterns, with the respective request" \
+    return "<h1>API Sandbox</h1>" \
+           "<p> To play around with the APIs, follow the guide below</p>" \
            "<ul>" \
-           "<li> /add, POST" \
+           "<li> /add, POST, (request body required)" \
            "<li> /showAll, GET" \
+           "<li> /update/<id>, PUT" \
+           "<li> /delete/<id>, DELETE" \
            "</ul>"
-
-
-@app.route('/tutor', methods=['GET'])
-def get_tutor():
-    return {
-        "name": "omruti",
-        "fav subjects":
-            [
-                "research",
-                "data science",
-                "sql",
-                "literature"
-            ]
-    }
 
 
 @app.route('/add', methods=['POST'])
 def add_to_db():
     obj = request.get_json()
     sandbox.add(obj)
-    return obj
+    return jsonify(sandbox.find_by_criteria(obj))
 
 
 @app.route('/showAll', methods=['GET'])
 def show_collections():
     return jsonify(sandbox.find_all())
+
+
+@app.route('/update/<_id>/<k>/<v>', methods=['PUT'])
+def update_by_id(_id, k, v):
+    sandbox.update_by_id(_id, key=k, value=v)
+    return jsonify(sandbox.find_by_id(_id))
+
+
+@app.route('/delete/<_id>', methods=['DELETE'])
+def delete_by_id(_id):
+    deleted = sandbox.find_by_id(_id)
+    sandbox.remove_by_id(_id)
+    return jsonify(deleted)
 
 
 if __name__ == '__main__':
